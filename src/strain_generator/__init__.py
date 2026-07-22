9f9d2f407ec6ec0309b0582babb34064cc9653b4
andrewlapointe <andrewrlapointe@gmail.com>
2026-07-22 10:49:49 -0400

    package config fix

--- src/strain_generator/__init__.py
+++ src/strain_generator/__init__.py
@@ -1,9 +1,13 @@
 import logging
 import logging.config
 import yaml
+from importlib.resources import files
 
-with open("config/logging_config.yaml", "r") as log_f, \
-     open("config/package.yaml", "r") as package_f:
+logger_config_file = files("strain_generator.config").joinpath("logging_config.yaml")
+package__config_file = files("strain_generator.config").joinpath("package_config.yaml")
+
+with open(logger_config_file, "r") as log_f, \
+     open(package__config_file, "r") as package_f:
 
     logger_config = yaml.safe_load(log_f.read())
     logging.config.dictConfig(logger_config)
