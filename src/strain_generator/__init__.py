3f6dcf777afda7e3a73cec07ed2e9daf4fe9dd39
andrewlapointe <andrewrlapointe@gmail.com>
2026-07-22 10:52:06 -0400

    fixed file path

--- src/strain_generator/__init__.py
+++ src/strain_generator/__init__.py
@@ -4,10 +4,10 @@ import yaml
 from importlib.resources import files
 
 logger_config_file = files("strain_generator.config").joinpath("logging_config.yaml")
-package__config_file = files("strain_generator.config").joinpath("package_config.yaml")
+package_config_file = files("strain_generator.config").joinpath("package.yaml")
 
 with open(logger_config_file, "r") as log_f, \
-     open(package__config_file, "r") as package_f:
+     open(package_config_file, "r") as package_f:
 
     logger_config = yaml.safe_load(log_f.read())
     logging.config.dictConfig(logger_config)
